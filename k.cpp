f01d6a779408ea5e45726c325e2345991df7a3b7
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-10-05 18:08:40 +0600

    basic swaping value

---
+++ k.cpp
@@ -0,0 +1,12 @@
+//Write a code to swap the value of two variables
+
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a = 1, b = 2, temp = a;
+    a = b; 
+    b = temp;
+    cout<< a;
+}
