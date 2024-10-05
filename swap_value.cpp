b24b0c958f6a0a44feaa1d25f70fc7b41cf89bf9
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-10-05 19:15:44 +0600

    changes done

---
+++ swap_value.cpp
@@ -0,0 +1,12 @@
+//Write a code to swap the value of two variables
+
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a = 4, b = 3, temp = a;
+    a = b; 
+    b = temp;
+    cout<< a;
+}
