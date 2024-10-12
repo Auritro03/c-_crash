2dfc9df6da06441fdf901a8280348798c7455ba5
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2024-10-12 10:43:08 +0600

    Simple array solved

---
+++ array.cpp
@@ -0,0 +1,15 @@
+//Simple array 
+#include <iostream>
+using namespace std;
+
+int main (){
+
+// int num[] = {10, 12, 13, 15, 16, 18};
+string phn[] = {"sam", "sung", "app", "wal", "pix"};
+
+
+cout<< phn[3];
+return 0; 
+
+
+}
